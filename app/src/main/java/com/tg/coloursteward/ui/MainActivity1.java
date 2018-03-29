65865f46c49c1b671c82f15a091df0e61d292f8c
fengyw <ywfengyw@gmail.com>
2018-03-29 14:01:21 +0800

    沟通

--- app/src/main/java/com/tg/coloursteward/ui/MainActivity1.java
+++ app/src/main/java/com/tg/coloursteward/ui/MainActivity1.java
@@ -106,6 +106,7 @@ public class MainActivity1 extends AppCompatActivity {
         //mTabLayout.setupWithViewPager(mViewPager);
 
         mViewPager.setCurrentItem(0);
+        mViewPager.setOffscreenPageLimit(3);
 
         tv_title.setText(navigation.getMenu().getItem(0).getTitle());
     }

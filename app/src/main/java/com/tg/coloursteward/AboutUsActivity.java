6fd26f85437fdd7a19068b31574aef89816a2f76
lizc <510906433@qq.com>
2018-08-29 10:42:49 +0800

    commit

--- app/src/main/java/com/tg/coloursteward/AboutUsActivity.java
+++ app/src/main/java/com/tg/coloursteward/AboutUsActivity.java
@@ -1,40 +1,41 @@
 package com.tg.coloursteward;
 
-import com.tg.coloursteward.base.BaseActivity;
-import com.tg.coloursteward.updateapk.UpdateManager;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.tg.coloursteward.base.BaseActivity;
+import com.tg.coloursteward.updateapk.UpdateManager;
+
 /**
- * 关于app
- * 
+ * 关于app页面
+ *
  * @author Administrator
- * 
  */
 public class AboutUsActivity extends BaseActivity {
-	private TextView tvVersionShort;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		tvVersionShort=(TextView) findViewById(R.id.tv_versionShort);
-		/**
-		 * 获取版本号
-		 */
-		String versionShort = UpdateManager.getVersionName(AboutUsActivity.this);
-		tvVersionShort.setText("V "+versionShort);
-	}
+    private TextView tvVersionShort;
 
-	@Override
-	public View getContentView() {
-		// TODO Auto-generated method stub
-		return getLayoutInflater().inflate(R.layout.activity_about_us, null);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        tvVersionShort = (TextView) findViewById(R.id.tv_versionShort);
+        /**
+         * 获取版本号
+         */
+        String versionShort = UpdateManager.getVersionName(AboutUsActivity.this);
+        tvVersionShort.setText("V " + versionShort);
+    }
 
-	@Override
-	public String getHeadTitle() {
-		// TODO Auto-generated method stub
-		return "关于彩管家";
-	}
+    @Override
+    public View getContentView() {
+        // TODO Auto-generated method stub
+        return getLayoutInflater().inflate(R.layout.activity_about_us, null);
+    }
+
+    @Override
+    public String getHeadTitle() {
+        // TODO Auto-generated method stub
+        return "关于彩管家";
+    }
 
 }

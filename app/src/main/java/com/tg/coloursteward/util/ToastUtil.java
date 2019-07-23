3403aa16f416afc9fb64b46710a32182de79d55a
lizc <510906433@qq.com>
2019-07-23 21:01:03 +0800

    修改Toast

--- app/src/main/java/com/tg/coloursteward/util/ToastUtil.java
+++ app/src/main/java/com/tg/coloursteward/util/ToastUtil.java
@@ -16,16 +16,8 @@ public class ToastUtil {
      * @param msg 显示的内容-字符串
      */
     public static void showShortToast(Context context, String msg) {
-        if (null != context && !TextUtils.isEmpty(msg) && isshow == true) {
-            if (toast == null) {
-                toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
-            } else {
-                toast.setText(msg);
-            }
-            //1、setGravity方法必须放到这里，否则会出现toast始终按照第一次显示的位置进行显示（比如第一次是在底部显示，那么即使设置setGravity在中间，也不管用）
-            //2、虽然默认是在底部显示，但是，因为这个工具类实现了中间显示，所以需要还原，还原方式如下：
-            toast.setGravity(Gravity.BOTTOM, 0, dip2px(context, 64));
-            toast.show();
+        if (null != context && !TextUtils.isEmpty(msg) && isshow) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
         }
     }
 

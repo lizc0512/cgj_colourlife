82d32b47b1fecf908865f56ee22ec925d159c604
lizc <510906433@qq.com>
2019-09-05 16:35:44 +0800

    新增网络状态判断

---
+++ app/src/main/java/com/tg/coloursteward/inter/NetStatusListener.java
@@ -0,0 +1,15 @@
+package com.tg.coloursteward.inter;
+
+/**
+ * @name 网络监听
+ * @class name：com.tg.coloursteward.inter
+ * @class describe
+ * @anthor 网络监听 QQ:510906433
+ * @time 2019/9/5 15:49
+ * @change
+ * @chang time
+ * @class describe
+ */
+public interface NetStatusListener {
+    void netChangeStatus(int status);
+}

1865eec522f64f26b7cbad3e0c44dc68e9f96241
lizc <510906433@qq.com>
2019-09-09 17:29:41 +0800

    修复高版本升级无法弹出问题

--- app/src/main/java/com/tg/coloursteward/serice/UpdateService.java
+++ app/src/main/java/com/tg/coloursteward/serice/UpdateService.java
@@ -174,7 +174,7 @@ public class UpdateService extends Service {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             if (Build.VERSION.SDK_INT > 23) { //判读版本是否在7.0含以上
                 //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
-                Uri apkUri = FileProvider.getUriForFile(context, "com.colourlife.safelife.fileprovider", file);
+                Uri apkUri = FileProvider.getUriForFile(context, "com.tg.coloursteward.fileprovider", file);
                 //添加这一句表示对目标应用临时授权该Uri所代表的文件
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
@@ -200,7 +200,7 @@ public class UpdateService extends Service {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             if (Build.VERSION.SDK_INT > 23) { //判读版本是否在7.0含以上
                 //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
-                Uri apkUri = FileProvider.getUriForFile(context, "com.colourlife.safelife.fileprovider", file);
+                Uri apkUri = FileProvider.getUriForFile(context, "com.tg.coloursteward.fileprovider", file);
                 //添加这一句表示对目标应用临时授权该Uri所代表的文件
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 intent.setDataAndType(apkUri, "application/vnd.android.package-archive");

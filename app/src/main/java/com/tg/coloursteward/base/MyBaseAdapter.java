b36f7af944d3260381c7796bb0f0cf76b8a2cd12
lizc <510906433@qq.com>
2019-09-10 11:00:27 +0800

    去除无用entity

--- app/src/main/java/com/tg/coloursteward/base/MyBaseAdapter.java
+++ app/src/main/java/com/tg/coloursteward/base/MyBaseAdapter.java
@@ -18,19 +18,16 @@ public abstract class MyBaseAdapter<T> extends BaseAdapter {
 
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
         return list == null ? 0 : list.size();
     }
 
     @Override
     public Object getItem(int position) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
         return position;
     }
 }

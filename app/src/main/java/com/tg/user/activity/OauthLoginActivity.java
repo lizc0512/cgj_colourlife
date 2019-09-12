5a6bffca012ce45e0299ee62b97175cbe99d1a10
袁松凯 <827194927@qq.com>
2019-09-12 16:18:13 +0800

    彩之云授权页添加返回按钮

--- app/src/main/java/com/tg/user/activity/OauthLoginActivity.java
+++ app/src/main/java/com/tg/user/activity/OauthLoginActivity.java
@@ -15,6 +15,7 @@ import com.tg.coloursteward.util.GlideUtils;
  * 彩之云授权获取用户登录信息后进行登录
  */
 public class OauthLoginActivity extends BaseActivity {
+    private ImageView iv_return_back;
     private ImageView iv_head_pic;
     private TextView tv_user_name;
     private Button btn_login;
@@ -41,9 +42,11 @@ public class OauthLoginActivity extends BaseActivity {
 
     private void initView() {
         iv_head_pic = findViewById(R.id.iv_head_pic);
+        iv_return_back = findViewById(R.id.iv_return_back);
         tv_user_name = findViewById(R.id.tv_user_name);
         btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(singleListener);
+        iv_return_back.setOnClickListener(singleListener);
         Intent intent = getIntent();
         String czy_name = intent.getStringExtra("czy_name");
         String czy_portrait = intent.getStringExtra("czy_portrait");
@@ -60,6 +63,9 @@ public class OauthLoginActivity extends BaseActivity {
                 Intent it = new Intent(OauthLoginActivity.this, LoginActivity.class);
                 it.putExtra("czy_code", czy_code);
                 startActivity(it);
+                finish();
+                break;
+            case R.id.iv_return_back:
                 finish();
                 break;
         }

e64c4a77bfe566aa4e8212940ee25f07c20a7599
袁松凯 <827194927@qq.com>
2019-09-16 16:34:14 +0800

    修改登录相关的UI问题

--- app/src/main/java/com/tg/coloursteward/util/TokenUtils.java
+++ app/src/main/java/com/tg/coloursteward/util/TokenUtils.java
@@ -11,6 +11,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -527,4 +528,46 @@ public class TokenUtils {
         }
         return paramsMap;
     }
+
+    public static boolean isMIUI() {
+        String manufacturer = Build.MANUFACTURER;
+        if ("xiaomi".equalsIgnoreCase(manufacturer)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isEMUI() {
+        String manufacturer = Build.MANUFACTURER;
+        if ("HUAWEI".equalsIgnoreCase(manufacturer)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isOPPO() {
+        String manufacturer = Build.MANUFACTURER;
+        if ("OPPO".equalsIgnoreCase(manufacturer)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isVIVO() {
+        String manufacturer = Build.MANUFACTURER;
+        if ("vivo".equalsIgnoreCase(manufacturer)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取当前手机系统版本号
+     *
+     * @return  系统版本号
+     */
+    public static int getSystemVersion() {
+        return Integer.parseInt(Build.VERSION.RELEASE);
+    }
+
 }

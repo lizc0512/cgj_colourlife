60e25d6929d96d766d903dff1d870fc53feb81a8
lizc <510906433@qq.com>
2019-09-17 09:38:05 +0800

    清理

--- app/src/main/java/com/tg/coloursteward/net/HttpTools.java
+++ app/src/main/java/com/tg/coloursteward/net/HttpTools.java
@@ -68,7 +68,6 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class HttpTools {
-    public static final String DIFFERENCE = "difference";
     public static final int RESPONSE_ERROR = 0;
     public static final int RESPONSE_SUCCES = 1;
     public static final int RESPONSE_START = 2;
@@ -83,141 +82,34 @@ public class HttpTools {
     public static final String FIELD_RESULT = "result";
     public static final String FIELD_MESSAGE = "message";
     public static final String FIELD_CONTENT = "content";
-    public static final String FIELD_TOTAL_COUNT = "total";
     public static final String FIELD_DATA = "data";
     public static int TIME_OUT = 30000;
 
     private static int BASE_CODE = 1;
     public static final int POST_IMAG = BASE_CODE++;
-    public static final int GET_TS = BASE_CODE++;
-    public static final int GET_SKIN_INFO = BASE_CODE++;
-    public static final int GET_CZY_ID = BASE_CODE++;
     public static final int GET_ACCOUNT_INFO = BASE_CODE++;
-    public static final int GET_APP = BASE_CODE++;
-    public static final int GET_LOGIN = BASE_CODE++;
     public static final int GET_USER_INFO = BASE_CODE++;
-    public static final int GET_USER_JJB = BASE_CODE++;
-    public static final int GET_USER_JJB_DETAIL = BASE_CODE++;
-    public static final int GET_VERSION_INFO = BASE_CODE++;
     public static final int POST_DELETE_INFO = BASE_CODE++;
-    public static final int SET_REPAIR_INFO = BASE_CODE++;
-    public static final int GET_HOME_LSIT = BASE_CODE++;
-    public static final int SET_COMPLAINT_INFO = BASE_CODE++;
-    public static final int SET_USER_INFO = BASE_CODE++;
-    public static final int SET_OPINION_INFO = BASE_CODE++;
-    public static final int SET_PASSWORD = BASE_CODE++;
     public static final int GET_EMPLOYEE_INFO = BASE_CODE++;
-    public static final int DELETE_EMPLOYEE_INFO = BASE_CODE++;
-    public static final int SET_EMPLOYEE_INFO = BASE_CODE++;
-    public static final int GET_ACCOUNT_TEMP = BASE_CODE++;
-    public static final int GET_PASSWORD_INFO = BASE_CODE++;
-    public static final int POST_CLEAR_PAYPWD = BASE_CODE++;
-    public static final int GET_USER_INITORG = BASE_CODE++;
-    public static final int GET_POI_COMMUNITY = BASE_CODE++;
-    public static final int GET_KPI_INFO = BASE_CODE++;
-    public static final int GET_STATISTICS_INFO = BASE_CODE++;
-    public static final int GET_MAP_LIST = BASE_CODE++;
-    public static final int GET_KPI_YEAR = BASE_CODE++;
-    public static final int GET_KPI_YEAR_INFO = BASE_CODE++;
-    public static final int GET_SEA_HEALTH = BASE_CODE++;
-    public static final int GET_BONUS_RECORD = BASE_CODE++;
-    public static final int GET_BALANCE_INFO = BASE_CODE++;
-    public static final int GET_RESULT = BASE_CODE++;
-    public static final int POST_SETPWD_INFO = BASE_CODE++;
-    public static final int GET_HBUSER_LIST = BASE_CODE++;
     public static final int GET_HBUSER_MONEY = BASE_CODE++;
     public static final int GET_DGZH_MONEY = BASE_CODE++;
-    public static final int GET_BANK_LIST = BASE_CODE++;
-    public static final int DETELE_BANK_ITEM = BASE_CODE++;
-    public static final int POST_SEND_PACKET = BASE_CODE++;
-    public static final int DETELE_HOME_LIST = BASE_CODE++;
-    public static final int DELETE_CAY_INFO = BASE_CODE++;
     public static final int SET_PWD_INFO = BASE_CODE++;
     public static final int SET_CHECK_PWD = BASE_CODE++;
-    public static final int GET_AUTHORIZATION_LIST = BASE_CODE++;
-    public static final int POST_APPLY_MOBILE = BASE_CODE++;
-    public static final int POST_CARRY_ORDER = BASE_CODE++;
-    public static final int POST_BAND_BANK = BASE_CODE++;
-    public static final int POST_CUSTOMER_INFO = BASE_CODE++;
-    public static final int POST_SMS_INFO = BASE_CODE++;
-    public static final int PUT_SMS_INFO = BASE_CODE++;
-    public static final int POST_BAND_CZY = BASE_CODE++;
-    public static final int GET_REAL_MONEY = BASE_CODE++;
-    public static final int PSOT_WETOWN_INFO = BASE_CODE++;
-    public static final int PSOT_OTHER_INFO = BASE_CODE++;
-    public static final int PSOT_COMMUNITY_INFO = BASE_CODE++;
-    public static final int PSOT_LOGWETOWN_INFO = BASE_CODE++;
-    public static final int PSOT_CLEARLOGWETOWN_INFO = BASE_CODE++;
-    public static final int GET_DOOR_GRANTED = BASE_CODE++;
-    public static final int GET_CHECK_OPEN = BASE_CODE++;
-    public static final int GET_ADD_OPEN = BASE_CODE++;
-    public static final int POST_LINE_LOG = BASE_CODE++;
-    public static final int GET_DOOR_MESSAGE = BASE_CODE++;
-    public static final int GET_OPEN_DOOR = BASE_CODE++;
-    public static final int PSOT_APPLY_INFO = BASE_CODE++;
-    public static final int PSOT_COUNT_INFO = BASE_CODE++;
-    public static final int PSOT_DOORAPPLY_INFO = BASE_CODE++;
-    public static final int GET_SCAN_INFO = BASE_CODE++;
-    public static final int GET_URL = BASE_CODE++;
-    public static final int GET_TYPE_LIST = BASE_CODE++;
-    public static final int PSOT_ADDFIXED_INFO = BASE_CODE++;
-    public static final int PSOT_DELETEFIXED_INFO = BASE_CODE++;
-    public static final int PSOT_MODIFY_INFO = BASE_CODE++;
-    public static final int PSOT_INDEX_INFO = BASE_CODE++;
-    public static final int POST_AUTOR_INFO = BASE_CODE++;
-    public static final int GET_AUTOR_INFO = BASE_CODE++;
-    public static final int GET_COMMUNITY_INFO = BASE_CODE++;
-    public static final int POST_APPLY_INFO = BASE_CODE++;
-    public static final int POST_AUTHOR_INFO = BASE_CODE++;
     public static final int GET_ACCOUNT_LIST = BASE_CODE++;
     public static final int GET_APPS_DETAIL = BASE_CODE++;
-    public static final int POST_VACCOUN_VERIFY = BASE_CODE++;
-    public static final int GET_AD_LIST = BASE_CODE++;
-    public static final int SET_MSG_READ = BASE_CODE++;
-    public static final int GET_UPDATE_PUSH = BASE_CODE++;
-    public static final int GET_ORG_TYPE = BASE_CODE++;
-    public static final int POST_WITHDRAWALS = BASE_CODE++;
     public static final int POST_FASTTRANSACTION = BASE_CODE++;
     public static final int GET_ORG_PAGE = BASE_CODE++;
-    public static final int GET_ACCESS_TOKEN = BASE_CODE++;
-    public static final int GET_REGISTER_ACCOUNT = BASE_CODE++;
-    public static final int GET_TENANT_LIST = BASE_CODE++;
-    public static final int GET_FINACE_BYOA = BASE_CODE++;
     public static final int GET_FINACE_BYOA_ONE = BASE_CODE++;
     public static final int GET_FINACE_BYOA_MULTI = BASE_CODE++;
-    public static final int GET_EXAMINE1 = BASE_CODE++;
-    public static final int GET_EXAMINE2 = BASE_CODE++;
-    public static final int GET_FINANCEBYOA = BASE_CODE++;
-    public static final int POST_OAUTH_TOKEN = BASE_CODE++;
-    public static final int POST_OAUTH_RETOKEN = BASE_CODE++;
-    public static final int DETELE_CONTACT_LIST = BASE_CODE++;
-    public static final int POST_SINGLEDEVICE = BASE_CODE++;
-    public static final int POST_LOGOUTDEVICE = BASE_CODE++;
     public static final int GET_SPLIT_BILL_DETAIL = BASE_CODE++;
-    public static final int GET_FRAGMENTMINE = BASE_CODE++;
-    public static final int POST_TWOJIYAN = BASE_CODE++;
-    public static final int GET_MYPAGERULE = BASE_CODE++;
     public static final int GET_DETAILRULE = BASE_CODE++;
-    public static final int POST_CHANGEPHONE_MMS = BASE_CODE++;
-    public static final int POST_CHANGEPHONE_COMMIT = BASE_CODE++;
-    public static final int GET_CZYOAUTH = BASE_CODE++;
-    public static final int GET_MINISERVER = BASE_CODE++;
     public static final int GET_MINISERVER_TOP = BASE_CODE++;
-    public static final int POST_SCAN = BASE_CODE++;
-    public static final int POST_USERSYNC = BASE_CODE++;
-    public static final int GET_FP = BASE_CODE++;
-    public static final int GET_DIALOG = BASE_CODE++;
     public static final int GET_YINGYAN = BASE_CODE++;
     public static final int GET_AD = BASE_CODE++;
-
-
-    public static final int GET_KEYSECERT = -10000;
 
     private static RequestQueue mQueue;
     private static HttpClient postClient = null;
     private static boolean cancelPost = false;
-    private static boolean test = false;
-    private static String color_token;
 
     public static RequestQueue getRequestQueue() {
         if (mQueue == null) {
@@ -570,31 +462,6 @@ public class HttpTools {
     }
 
     /**
-     * 上传多张
-     *
-     * @param hand
-     * @param imgParams
-     */
-    public static void postImages(final String URL, final Handler hand,
-                                  final ArrayList<ImageParams> imgParams) {
-        cancelPost = false;
-        new Thread() {
-            public void run() {
-                ImageParams imgParam;
-                boolean isLast = false;
-                int len = imgParams.size();
-                for (int i = 0; i < len; i++) {
-                    isLast = i == len - 1;
-                    imgParam = imgParams.get(i);
-                    if (!postAnImg(URL, isLast, hand, imgParam)) {
-                        return;
-                    }
-                }
-            }
-        }.start();
-    }
-
-    /**
      * 上传一张
      *
      * @param hand
@@ -874,109 +741,6 @@ public class HttpTools {
         get(url, Method.DELETE, rqtConfig);
     }
 
-    /***请求Combile的get加密***/
-    public static String getCombileUrl(String urlString, Map<String, Object> paramsMap) {
-        String version = "1.0.0";
-        String sign = null;
-        Map<String, String> stringMap = getStringMap(paramsMap);
-        String keyValue = stringMap.get("key");
-        String secretValue = stringMap.get("secret");
-        stringMap.remove("key");
-        stringMap.remove("secret");
-        long diff = 0;
-        String ts = System.currentTimeMillis() / 1000 + diff + "";
-        String valueStr;
-        String secretString = getMapToString(stringMap);
-        if (TextUtils.isEmpty(secretString)) {
-            sign = urlString + "?" + "key=" + keyValue + "&ts=" + ts + "&ve=" + version + "&secret=" + secretValue;
-            valueStr = urlString + "?" + "key=" + keyValue;
-        } else {//有参数时
-            sign = urlString + secretString + "&key=" + keyValue + "&ts=" + ts + "&ve=" + version + "&secret=" + secretValue;
-            valueStr = urlString + secretString + "&key=" + keyValue;
-        }
-        valueStr = valueStr + "&ts=" + (System.currentTimeMillis() / 1000 + diff)
-                + "&ve=" + version + "&sign=" + setMD5(sign);
-        valueStr = valueStr.replaceAll(" ", "\\+");
-        return valueStr;
-    }
-
-    public static Map<String, String> getStringMap(Map<String, Object> paramsMap) {
-        Iterator<String> it = paramsMap.keySet().iterator();
-        Map<String, String> stringMap = new HashMap<>();
-        while (it.hasNext()) {
-            String key = it.next();
-            String value = String.valueOf(paramsMap.get(key));
-            stringMap.put(key, value);
-        }
-        return stringMap;
-    }
-
-
-    /***请求Combile的post加密***/
-    private static String postCombileMD5(String urlString, Map<String, String> paramsMap) {
-        String version = "1.0.0";
-        String keyValue = paramsMap.get("key");
-        String secretValue = paramsMap.get("secret");
-        long diff = 0;
-        String ts = System.currentTimeMillis() / 1000 + diff + "";
-        String sign = urlString + "?" + "key=" + keyValue + "&ts=" + ts + "&ve=" + version + "&secret=" + secretValue;
-        String valueStr;
-        valueStr = urlString + "?" + "key=" + keyValue;
-        valueStr = valueStr + "&ts=" + (System.currentTimeMillis() / 1000 + diff)
-                + "&ve=" + version + "&sign=" + setMD5(sign);
-        return valueStr;
-    }
-
-    public static String getMapToString(Map<String, String> paramsMap) {
-        if (null == paramsMap || paramsMap.size() == 0) {
-            return "";
-        } else {
-            Iterator<String> it = paramsMap.keySet().iterator();
-            StringBuffer sb = null;
-            while (it.hasNext()) {
-                String key = it.next();
-                String value = String.valueOf(paramsMap.get(key));
-                if (sb == null) {
-                    sb = new StringBuffer();
-                    sb.append("?");
-                } else {
-                    sb.append("&");
-                }
-                sb.append(key);
-                sb.append("=");
-                sb.append(value);
-            }
-            if (null == sb) {
-                return "";
-            } else {
-                return sb.toString();
-            }
-        }
-    }
-
-
-    /***md5加密***/
-    public static String setMD5(String string) {
-        MessageDigest md5;
-        StringBuilder sb = new StringBuilder();
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-            md5.update(string.getBytes("UTF-8"));
-            byte[] b = md5.digest();
-            for (byte aB : b) {
-                int temp = 0xFF & aB;
-                String s = Integer.toHexString(temp);
-                if (temp <= 0x0F) {
-                    s = "0" + s;
-                }
-                sb.append(s);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
-
     /**
      * Post请求
      *
@@ -993,24 +757,6 @@ public class HttpTools {
             paramsStr = requestParams.toHashMapString();
         }
         post(URL + apiName, Method.POST, rqtConfig, paramsStr);
-    }
-
-    /**
-     * Put请求
-     *
-     * @param URL
-     * @param apiName
-     * @param rqtConfig
-     * @param requestParams
-     */
-    public static void httpPut(final String URL, String apiName, final RequestConfig rqtConfig, RequestParams requestParams) {
-        HashMap<String, String> paramsStr = null;
-        if (requestParams == null) {
-            paramsStr = null;
-        } else {
-            paramsStr = requestParams.toHashMapString();
-        }
-        post(URL + apiName, Method.PUT, rqtConfig, paramsStr);
     }
 
     /**
@@ -1074,29 +820,6 @@ public class HttpTools {
             return null;
         }
         return jsonObj.optString(FIELD_MESSAGE, null);
-    }
-
-    /**
-     * 获取图片名字
-     *
-     * @param jsonString
-     * @return
-     */
-    public static String getFileNameString(String jsonString) {
-        if (TextUtils.isEmpty(jsonString)) {
-            return null;
-        }
-        JSONObject jsonObj = null;
-        try {
-            jsonObj = new JSONObject(jsonString);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
-        }
-        if (jsonObj.isNull("filename")) {
-            return null;
-        }
-        return jsonObj.optString("filename", null);
     }
 
     /**
@@ -1204,27 +927,6 @@ public class HttpTools {
             return jsonArray;
         }
 
-    }
-
-    /**
-     * 获取total信息
-     *
-     * @param jsonString
-     * @return
-     */
-    public static int getTotalCount(String jsonString) {
-        int count = 0;
-        if (TextUtils.isEmpty(jsonString)) {
-            return count;
-        }
-        JSONObject jsonObj = null;
-        try {
-            jsonObj = new JSONObject(jsonString);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return count;
-        }
-        return jsonObj.optInt(FIELD_TOTAL_COUNT, count);
     }
 
     /**
@@ -1393,27 +1095,6 @@ public class HttpTools {
         }
         SparseArray<HashMap<String, Object>> sparseArray = new SparseArray<HashMap<String, Object>>();
         sparseArray.put(0, getMap(jsonObj));
-        return new ResponseData(sparseArray);
-    }
-
-    private static ResponseData parseJsonString(String jsonString) {
-        if (TextUtils.isEmpty(jsonString)) {
-            return new ResponseData(null);
-        }
-        JSONArray jsonArray = null;
-        try {
-            jsonArray = new JSONArray(jsonString);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return new ResponseData(null);
-        }
-        SparseArray<HashMap<String, Object>> sparseArray = new SparseArray<HashMap<String, Object>>();
-        int len = jsonArray.length();
-        JSONObject jsonObj;
-        for (int i = 0; i < len; i++) {
-            jsonObj = jsonArray.optJSONObject(i);
-            sparseArray.put(i, getMap(jsonObj));
-        }
         return new ResponseData(sparseArray);
     }
 
